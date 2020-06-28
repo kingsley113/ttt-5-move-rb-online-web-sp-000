d7af49f71b95f9497db97d252991ff6c43c6f41e
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-06-28 02:53:51 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ def input_to_index(user_input)
   index = user_input.to_i - 1
 end
 
-def move(array, index.to_i, value = "X")
-  array[index] = value
+def move(array, index, value = "X")
+  array[index.to_i] = value
 end
